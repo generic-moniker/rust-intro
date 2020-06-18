f942f50c312270ef88cc6b726c30d2f4c7b2753e
generic-moniker <danialchowdhry@hotmail.com>
2020-06-17 21:10:54 -0700

    Unit 1.2

---
+++ unit1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\dania\Projects\rust-intro\unit1\hello_cargo\target\debug\hello_cargo.exe: C:\Users\dania\Projects\rust-intro\unit1\hello_cargo\src\main.rs
---
+++ unit1/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\dania\Projects\rust-intro\unit1\hello_cargo\target\release\hello_cargo.exe: C:\Users\dania\Projects\rust-intro\unit1\hello_cargo\src\main.rs
