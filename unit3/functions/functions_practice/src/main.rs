70ffa33eb35277f3404d5812d877632bb4d58262
generic-moniker <danialchowdhry@hotmail.com>
2020-06-23 18:52:55 -0700

    adding functions notes

---
+++ unit3/functions/functions_practice/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    another_function(5, 6);
+    let x = five();
+    let y = plus_one(x);
+    println!("The value of a is: {}", x);
+    println!("The value of b is: {}", y);
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
